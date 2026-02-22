2b57457f76bcd918da1799000f5341c8a73ca53b
pupavajuraj3-cyber <pupavajuraj3@gmail.com>
2026-02-22 20:14:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
 import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
@@ -11,12 +11,13 @@ def index():
             hrubka = float(request.form.get('hrubka', 0))
             polomer = float(request.form.get('polomer', 0))
             uhol = float(request.form.get('uhol', 0))
+            # Tvoj vypocet
             vysledok = round((hrubka + polomer) * (uhol / 90), 2)
         except:
             vysledok = "Chyba: Zadaj správne čísla"
+    
     return render_template('index.html', vysledok=vysledok)
 
 if __name__ == '__main__':
-    # Tato cast je klucova pre Render:
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)

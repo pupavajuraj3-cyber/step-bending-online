f7a9ab68e1290b613a98ca5153ae932e1642a55a
pupavajuraj3-cyber <pupavajuraj3@gmail.com>
2026-02-22 20:24:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, render_template, request
 
-# Povieme Flasku, kde presne hladat HTML subory
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,19 +8,19 @@ def index():
     vysledok = None
     if request.method == 'POST':
         try:
-            # Nacitame hodnoty z formulara
+            # Načítanie hodnôt z formulára
             hrubka = float(request.form.get('hrubka', 0))
             polomer = float(request.form.get('polomer', 0))
             uhol = float(request.form.get('uhol', 0))
             
-            # Tvoj zakladny vypocet (upravime neskor podla tvojej praxe)
+            # Výpočet: (Hrúbka + Polomer) * (Uhol / 90)
+            # Sem neskôr vložíme tvoj presný vzorec
             vysledok = round((hrubka + polomer) * (uhol / 90), 2)
         except:
-            vysledok = "Chyba: Zadaj správne čísla"
+            vysledok = "Chyba: Zadaj čísla!"
             
     return render_template('index.html', vysledok=vysledok)
 
 if __name__ == '__main__':
-    # Nastavenie portu, aby Render vedel aplikaciu spustit
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
